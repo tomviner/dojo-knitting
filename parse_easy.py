40e7843405a0e453be8ea46046443bba8e5d4998
Jian Yuan Lee <jianyuan@gmail.com>
2016-09-08 21:23:20 +0100

    works

--- parse_easy.py
+++ parse_easy.py
@@ -12,6 +12,16 @@ map = {
 for row, line in enumerate(pattern, 1):
     line = line[:-1]
     print 'Line', row
+    count, last = 0, None
     for col, char in enumerate(line, 1):
-        print '\tdo a', map.get(char, repr(char))
+        if last is None:
+            count = 1
+        else:
+            if last != char:
+                print '\tdo a', map.get(last, repr(last)), count
+                count = 1
+            else:
+                count += 1
+        last = char
 
+    print '\tdo a', map.get(char, repr(char)), ' x', count
