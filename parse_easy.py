93e135df46334cebd6a90c341fabb834585911ae
Rob Young <rob@robyoung.digital>
2016-09-08 21:12:39 +0100

    Add ability to go back by one line

--- parse_easy.py
+++ parse_easy.py
@@ -9,9 +9,17 @@ map = {
     '.': 'skip',
 }
 
-for row, line in enumerate(pattern, 1):
-    line = line[:-1]
-    print 'Line', row
-    for col, char in enumerate(line, 1):
-        print '\tdo a', map.get(char, repr(char))
+lines = [line.rstrip("\n") for line in pattern]
+row = 0
 
+while row < len(lines):
+    line = lines[row]
+    print "Line ", row + 1
+    for col, char in enumerate(line, 1):
+        print "\tdo a ", map.get(char, repr(char))
+
+    command = raw_input("> ")
+    if command == "u":
+        row -= 1
+    else:
+        row += 1
